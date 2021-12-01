2fc74c98e01046bbc84e13e97136f31516719542
Sebastian Olsson <me@sebastianolsson.com>
2021-12-01 09:47:55 +0100

    Cleanup and added the timer

--- 01/src/Main.cpp
+++ 01/src/Main.cpp
@@ -1,20 +1,22 @@
 #include "Utility.h"
+#include "Timer.h"
 
-void Sweep(std::vector<uint32_t>& depths, uint32_t window)
+uint32_t Sweep(std::vector<uint32_t>& depths, uint32_t window)
 {
+	Timer t;
 	uint32_t increases = 0;
 	for (uint32_t i = window; i < depths.size(); ++i)
 	{
 		if (depths[i - window] < depths[i])
 			++increases;
 	}
-	std::cout << "Increases: " << increases << "\n";
+	return increases;
 }
 
 int main()
 {
 	std::vector<uint32_t> depths = Utility::ReadFromFileToInt("../input.txt");
 
-	Sweep(depths, 1);
-	Sweep(depths, 3);
+	std::cout << "Part 1 = " << Sweep(depths, 1) << "\n";
+	std::cout << "Part 2 = " << Sweep(depths, 3) << "\n";
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2021
+
+## Day 1
+**Part 1** = (0.01009 ms) 1754 <br />
+**Part 2** = (0.0093 ms) 1789 <br />
