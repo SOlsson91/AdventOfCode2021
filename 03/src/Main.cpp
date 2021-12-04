c1a90f2cdd575ff84a1821b2265af0c13fb95146
Sebastian Olsson <me@sebastianolsson.com>
2021-12-04 12:54:51 +0100

    Day 3 P2. Didn't have time yesterday to work on the life support system. However got it solved today. Far from optimal but works

--- 03/src/Main.cpp
+++ 03/src/Main.cpp
@@ -34,8 +34,88 @@ uint32_t GetPowerConsumption(const std::vector<std::string>& input)
 	return Utility::StringToBinary(gammaRate) * Utility::StringToBinary(epsilonRate);
 }
 
+void RemoveRows(const std::vector<std::string>& toRemove, std::vector<std::string>& generatorRating)
+{
+	for (auto it = std::begin(generatorRating); it != std::end(generatorRating);)
+	{
+		bool removed = false;
+		for (const auto& removeItem : toRemove)
+		{
+			if (*it == removeItem)
+			{
+				generatorRating.erase(it);
+				removed = true;
+				break;
+			}
+		}
+		if (!removed)
+			++it;
+	}
+}
+
+uint64_t GetLifeSupportRating(const std::vector<std::string>& input)
+{
+	Timer t;
+	std::vector<std::string> oxygenGeneratorRating = input;
+	std::vector<std::string> co2ScrubberRating = input;
+
+	for (uint8_t i = 0; i != input[0].size(); i++)
+	{
+		std::vector<std::string> ones = {};
+		std::vector<std::string> zeroes = {};
+
+		if (oxygenGeneratorRating.size() > 1)
+		{
+			for (const std::string& row : oxygenGeneratorRating)
+			{
+				if (row[i] == '0')
+				{
+					zeroes.emplace_back(row);
+					continue;
+				}
+				ones.emplace_back(row);
+			}
+			if (ones.size() >= zeroes.size())
+			{
+				RemoveRows(zeroes, oxygenGeneratorRating);
+			}
+			else
+			{
+				RemoveRows(ones, oxygenGeneratorRating);
+			}
+
+		}
+
+		ones.clear();
+		zeroes.clear();
+		if (co2ScrubberRating.size() > 1)
+		{
+			for (const std::string& row : co2ScrubberRating)
+			{
+				if (row[i] == '0')
+				{
+					zeroes.emplace_back(row);
+					continue;
+				}
+				ones.emplace_back(row);
+			}
+			if (zeroes.size() <= ones.size())
+			{
+				RemoveRows(ones, co2ScrubberRating);
+			}
+			else
+			{
+				RemoveRows(zeroes, co2ScrubberRating);
+			}
+		}
+	}
+
+	return Utility::StringToBinary(oxygenGeneratorRating[0]) * Utility::StringToBinary(co2ScrubberRating[0]);
+}
+
 int main()
 {
 	std::vector<std::string> rows = Utility::ReadFromFileToString("../input.txt");
 	std::cout << "**Part 1** = " << GetPowerConsumption(rows) << "\n";
+	std::cout << "**Part 2** = " << GetLifeSupportRating(rows) << "\n";
 }
